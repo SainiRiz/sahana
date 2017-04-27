2b0d186f6328cd76e68df673335f0b9834dd23d7
SainiRiz <sahanababu96@gmail.com>
2017-04-27 16:25:13 +0530

    Create reverse2.c

---
+++ reverse2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int m,a=0,b,c;
+    scanf("%d",&m);
+    c=n;
+    while(n!=0)
+    {
+        b= m%10;
+        a=a*10+b;
+        m=m/10;
+    }
+    printf("%d",a);
+    return 0;
+}
