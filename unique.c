275bda76a19b0c2b679ed500652969453bed64ab
SainiRiz <sahanababu96@gmail.com>
2017-06-28 11:39:33 +0530

    Create unique.c

---
+++ unique.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+  int main()
+  {
+      int i,n,j,a[100],count=0;
+      scanf("%d",&n);
+      for(i=0;i<n;i++)
+          scanf("%d",&a[i]);
+      for(i=0;i<n;i++)
+      {
+          count=0;
+          for(j=0;j<n;j++)
+      {
+          if((a[i]==a[j])&&(i!=j))
+          {  count=1;
+              break;
+          }
+      }
+        if(count==0)
+            printf("%d",a[i]);
+          
+      }
+      return 0;
+  }
